b64372c23654842c5e94db60c1ae206abad8cbc1
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-02-17 15:21:38 +0100

    Add lifecycle diagram

--- js/static-codemirror.js
+++ js/static-codemirror.js
@@ -5,7 +5,6 @@ class StaticCodemirror extends HTMLElement {
     while (this.firstChild) {
       this.firstChild.remove();
     }
-    console.log(JSON.stringify(value));
     this._codeMirror = CodeMirror(
       element => {
         this.appendChild(element);
