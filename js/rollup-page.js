6a66e8e8c06d124d7cfd954f0dfbc63a994bb6cb
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-02-19 07:34:50 +0100

    Refine Rollup

--- js/rollup-page.js
+++ js/rollup-page.js
@@ -3,6 +3,10 @@ import { rollUpPage } from './rollup-integration.js';
 
 const configName = 'rollup.config.js';
 
+function wait() {
+  return new Promise(resolve => setTimeout(resolve));
+}
+
 class RollupPage extends HTMLElement {
   constructor() {
     super();
@@ -10,7 +14,10 @@ class RollupPage extends HTMLElement {
     const codeSample = codeSamples[this.getAttribute('code-sample')];
     this._createColumnContainer();
     this._createConfigColumn(codeSample.config);
-    this._input = this._createColumnFromCode(codeSample.input);
+    this._inputOutputColumn = document.createElement('div');
+    this._columnContainer.appendChild(this._inputOutputColumn);
+    this._input = this._createFilesFromCode(codeSample.input, this._inputOutputColumn);
+    this._output = {};
     this.addEventListener('keypress', event => event.stopPropagation());
     this._registerResizeHandler();
   }
@@ -22,67 +29,78 @@ class RollupPage extends HTMLElement {
   }
 
   _createConfigColumn(config) {
-    const configColumn = this._createColumnFromCode([{ fileName: configName, code: config }]);
-    this._config = configColumn.files[configName];
-    const rollupButton = document.createElement('button');
-    rollupButton.innerText = 'ROLLUP';
-    rollupButton.onclick = async () => {
-      this._input.column.remove();
-      if (this._output) {
-        this._removeColumn(this._output);
-      }
-      const output = await rollUpPage(this._config, this._input.files);
-      this._output = this._createColumnFromCode(output, true);
-      this._resizeCodeMirrors();
-    };
-    configColumn.column.appendChild(rollupButton);
-    const showInputButton = document.createElement('button');
-    showInputButton.innerText = 'SHOW INPUT';
-    showInputButton.onclick = async () => {
-      if (this._output) {
-        this._removeColumn(this._output);
-        this._output = null;
-      }
-      this._columnContainer.appendChild(this._input.column);
-    };
-    configColumn.column.appendChild(showInputButton);
+    const configColumn = document.createElement('div');
+    this._columnContainer.appendChild(configColumn);
+    this._config = this._createFilesFromCode(
+      [{ fileName: configName, code: config }],
+      configColumn
+    )[configName];
   }
 
-  _createColumnFromCode(codeSamples, isOutput) {
-    const column = document.createElement('div');
-    const output = {
-      column,
-      files: {}
-    };
+  _removeFileContainer(codeMirror, delay = 0) {
+    this._codeMirrors.delete(codeMirror);
+    const fileContainer = codeMirror.display.wrapper.parentElement;
+    fileContainer.classList.remove('zoom-in');
+    fileContainer.style.animationDelay = `0.${delay}s`;
+    fileContainer.clientHeight;
+    fileContainer.classList.add('zoom-out');
+    fileContainer.addEventListener('animationend', () => fileContainer.remove());
+  }
+
+  _createFilesFromCode(codeSamples, container) {
+    const files = {};
     for (const { fileName, code, source } of codeSamples) {
-      output.files[fileName] = this._addFileContainer(
-        column,
+      files[fileName] = this._addFileContainer(
+        container,
         fileName,
-        (typeof code === 'string' ? code : source).trim(),
-        isOutput
+        (typeof code === 'string' ? code : source).trim()
       );
     }
-    this._columnContainer.appendChild(column);
-    return output;
+    return files;
   }
 
-  _removeColumn(columnContent) {
-    for (const fileName of Object.keys(columnContent.files)) {
-      const codeMirror = columnContent.files[fileName];
-      if (!this._codeMirrors.has(codeMirror)) {
-        throw new Error(`Could not find codeMirror for ${fileName}`);
-      }
-      this._codeMirrors.delete(codeMirror);
-    }
-    columnContent.column.remove();
-  }
-
-  _addFileContainer(parent, fileName, code, isOutput) {
+  _addFileContainer(parent, fileName, code) {
     const fileContainer = document.createElement('div');
     fileContainer.setAttribute('class', 'file-container');
     fileContainer.innerHTML = `<label>${fileName}</label>`;
     parent.appendChild(fileContainer);
-    return this._createCodeMirror(fileContainer, code, isOutput);
+    return this._createCodeMirror(fileContainer, code);
+  }
+
+  async _updateOutput(codeSamples) {
+    let delay = 0;
+    const outputsToBeUpdated = new Set(Object.keys(this._output));
+    for (const { fileName, code, source } of codeSamples) {
+      const content = (typeof code === 'string' ? code : source).trim();
+      if (outputsToBeUpdated.has(fileName)) {
+        outputsToBeUpdated.delete(fileName);
+        this._output[fileName].setValue(content);
+        await wait();
+      } else {
+        this._output[fileName] = this._addOutputContainer(
+          this._inputOutputColumn,
+          fileName,
+          content,
+          delay++
+        );
+        await wait();
+      }
+    }
+    for (const fileName of outputsToBeUpdated) {
+      this._removeFileContainer(this._output[fileName], delay++);
+      delete this._output[fileName];
+      await wait();
+    }
+  }
+
+  _addOutputContainer(parent, fileName, code, outputDelay) {
+    const fileContainer = document.createElement('div');
+    fileContainer.setAttribute('class', 'file-container output');
+    fileContainer.innerHTML = `<label>${fileName}</label>`;
+    parent.appendChild(fileContainer);
+    fileContainer.classList.add('zoom-in');
+    fileContainer.style.animationDelay = `0.${outputDelay++}s`;
+    return this._createCodeMirror(fileContainer, code, true);
   }
 
   _createCodeMirror(parent, value, isOutput) {
@@ -95,12 +113,32 @@ class RollupPage extends HTMLElement {
         readOnly: isOutput && 'nocursor',
         scrollbarStyle: 'null',
         tabSize: 2,
-        theme: `default${isOutput ? ' rolled' : ''}`,
         value
       }
     );
     this._codeMirrors.add(codeMirror);
+    codeMirror.on('changes', () => this._handleCodeChanges());
     return codeMirror;
+  }
+
+  async _handleCodeChanges() {
+    if (this._updating) {
+      this._needsUpdate = true;
+    } else {
+      this._updating = true;
+      do {
+        this._needsUpdate = false;
+        await wait();
+        try {
+          const output = await rollUpPage(this._config, this._input);
+          await this._updateOutput(output);
+          this._resizeCodeMirrors();
+        } catch {
+          this._updateOutput([]);
+        }
+      } while (this._needsUpdate);
+      this._updating = false;
+    }
   }
 
   _registerResizeHandler() {
